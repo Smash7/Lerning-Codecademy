81765cf449ac0a0f6b5f268898c2f95f60d0667f
eugene-martynov <e.martynov@mforge.ru>
2023-04-27 16:02:09 +0200

    Release "The Nile"

--- Python3 Intermediate/The Nile/script.py
+++ Python3 Intermediate/The Nile/script.py
@@ -4,17 +4,48 @@ from test import test_function
 # Define calculate_shipping_cost() here:
 
 
+def calculate_shipping_cost(from_coords: tuple, to_coords: tuple, shipping_type='Overnight') -> str:
+    distance = get_distance(*from_coords, *to_coords)
+    shipping_rate = SHIPPING_PRICES[shipping_type]
+    price = distance * shipping_rate
+    return format_price(price)
+
+
+
+
+
 # Test the function by calling
-# test_function(calculate_shipping_cost)
+test_function(calculate_shipping_cost)
 
 # Define calculate_driver_cost() here
+def calculate_driver_cost(distance: int, *drivers):
+    cheapest_driver = None
+    cheapest_driver_price = None
+    for driver in drivers:
+        driver_time = distance / driver.speed
+        price_for_driver = driver.salary * driver_time
+        if cheapest_driver is None:
+            cheapest_driver = driver
+            cheapest_driver_price = price_for_driver
+        elif price_for_driver < cheapest_driver_price:
+            cheapest_driver = driver
+            cheapest_driver_price = price_for_driver
+    return cheapest_driver_price, cheapest_driver
 
 
 # Test the function by calling
-# test_function(calculate_driver_cost)
+test_function(calculate_driver_cost)
 
 # Define calculate_money_made() here
 
 
+def calculate_money_made(**trips):
+    total_money_made = 0
+    for trip_id, trip in trips.items():
+        trip_revenue = trip.cost - trip.driver.cost
+        total_money_made += trip_revenue
+    return total_money_made
+
+
 # Test the function by calling
-# test_function(calculate_money_made)
+test_function(calculate_money_made)
